509ca560fedc1f44aa274427adc465cacaa4ae5c
chu0802 <vul3qi@gmail.com>
2024-03-31 17:08:25 +0800

    <feat>: add resources page

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,84 +1,93 @@
-$(document).ready(function() {
-    function formatBytes(a,b=0,k=1024){
-        a = parseInt(a);
-        with(Math){let d=floor(log(a)/log(k));return 0==a?"0 Bytes":parseFloat((a/pow(k,d)).toFixed(max(0,b)))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
+document.addEventListener("DOMContentLoaded", function() {
+    // Function to format file size
+    function formatBytes(bytes, decimals = 0, k = 1024) {
+        if (bytes === 0) return "0 Bytes";
+        const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+        const i = Math.floor(Math.log(bytes) / Math.log(k));
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + " " + sizes[i];
     }
-    
-    function getReturnBlocks(root, e){
-        const img_type = ["jpg", "png"];
-        const icon_map = {
-            "": "file", "txt": "filetype-txt", "html": "file", 
-            "pdf": "file", "json": "file", "yaml": "file", "yml": "file", 
-            "gz": "file", "zip": "file", "ipynb": "notepad"};
 
-        var obj = new Object();
-        obj.parent_dir = e.text().includes("Parent Directory")? true : false;
-        obj.name = obj.parent_dir? "..." : e.text().slice(1);
-        obj.url = obj.parent_dir? e.attr('href') : root + e.attr('href');
-        obj.is_dir = obj.parent_dir || obj.name.at(-1) == "/"
-        obj.name = obj.is_dir ? obj.name.slice(0, -1) : obj.name;
-        obj.id = CryptoJS.MD5(obj.name).toString();
-        obj.size = "";
-        obj.date = "";
-        obj.type = obj.name.split('.').at(-1);
-        obj.is_img = img_type.some(el => obj.name.includes(el))? true : false;
-        obj.icon = obj.is_dir? "folder" : icon_map[obj.type];
-        if (e.attr('href') !== "/~r09922104/"){
-            $.ajax({
-                type: 'HEAD',
-                url: obj.url
-            }).done(function(msg, txt, jqXHR){
-                obj.size = obj.is_dir? "Directory" : formatBytes(jqXHR.getResponseHeader('Content-Length'));
-                obj.date = obj.is_dir? "" : new Date(jqXHR.getResponseHeader('Last-Modified')).toDateString();
-                $(`#${obj.id}`).append(
-                    `<small>${obj.size}<span class="date text-muted">${obj.date}</span></small>`
-                );
 
-                
-            });
-            return obj;
+    fetch('files_info.json').then(response => response.json()).then(files => {
+
+        const imgTypes = ["jpg", "png", "jpeg"];
+        const iconMap = {
+            "": "file", "txt": "filetype-txt", "html": "filetype-html",
+            "pdf": "file-pdf", "json": "filetype-json", "yaml": "filetype-yml", "yml": "filetype-yml",
+            "gz": "file-zip", "zip": "file-zip", "ipynb": "filetype-py"
+        };
+
+        function getDict(jsonObj, path) {
+            let value = jsonObj;
+            const keys = path.split("/"); // Split the path string by "/"
+            console.log(keys);
+            for (const key of keys) {
+                if (key === "resources"){
+                    continue;
+                }
+                if (value.hasOwnProperty(key)) {
+                    value = value[key]["files"];
+                } else {
+                    // Key not found, return undefined or handle the case as per your requirement
+                    return undefined;
+                }
+            }
+
+            return Object.entries(value);
         }
-        else
-            return null;
-    }
 
-    function get_files(root){
-        $('#files').empty();
-        $.ajax({url: root}).then(function(html) {
-            // create temporary DOM element
-            var document = $(html);
-            // find all links ending with .pdf 
-            document.find('a').each(function() {
-                var obj = getReturnBlocks(root, $(this));
-                if (obj)
-                    $("#files").append(
-                        `
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <div class="card">
-                                    <div class="file">
-                                        ${obj.is_dir ? `<a class="dir-link" href="javascript:void(0)" value="${obj.url}">` : `<a href="${obj.url}" download>`}
-                                            <div class="icon">
-                                                ${obj.is_img? `<img src="${obj.url}">` : `<i class="bx bx-${obj.icon} text-info"></i>`}
-                                            </div>
-                                            <div class="file-name" id="${obj.id}">
-                                                <p class="m-b-5 text-muted">${obj.name}</p>
-                                            </div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-                    );
+        function fetchFiles(fs) {
+            const filesContainer = document.getElementById('files');
+            filesContainer.innerHTML = '';
+            console.log(fs);
+            fs.forEach(([key, file]) => {
+                const metaDiv = document.createElement('div');
+                metaDiv.classList.add('col-lg-3', 'col-md-4', 'col-sm-6');
+
+                const cardDiv = document.createElement('div');
+                cardDiv.classList.add('card');
+                const fileDiv = document.createElement('div');
+                fileDiv.classList.add('file');
+                const linkElement = document.createElement('a');
+                const iconDiv = document.createElement('div');
+                iconDiv.classList.add('icon');
             
-            })
-        });
-    }
-    $("#files").on('click', '.dir-link', function(){
-        get_files($(this).attr("value"));
-        console.log($(this).attr("value"));
+                if (file.size === "Directory") {
+                    linkElement.classList.add('dir-link');
+                    linkElement.href = 'javascript:void(0)';
+                    fileDiv.addEventListener('click', function() {
+                        fetchFiles(getDict(files, file.url));
+                    });
+                    iconDiv.innerHTML = `<i class="bi bi-folder text-info"></i>`
+                }
+                else {
+                    if (imgTypes.includes(file.type.toLowerCase())) {
+                        iconDiv.innerHTML = `<img src="${file.url}" alt="${file.name}">`;
+                    }
+                    else {
+                        iconDiv.innerHTML = `<i class="bi bi-${iconMap[file.type.toLowerCase()]} text-info"></i>`;
+                    }
+                    console.log(file.size);
+                    linkElement.href = file.url;
+                    linkElement.download = file.name;
+                }
+                
+                const fileDetailsDiv = document.createElement('div');
+                fileDetailsDiv.classList.add('file-name');
+                fileDetailsDiv.innerHTML = `<p class="m-b-5 text-muted">${file.name}</p>`;
+                fileDetailsDiv.innerHTML += `<small>${file.size === "Directory"? file.size : formatBytes(file.size)}<span class="date text-muted">${file.last_modified}</span></small>`;
+
+                linkElement.appendChild(iconDiv);
+                linkElement.appendChild(fileDetailsDiv);
+                fileDiv.appendChild(linkElement);
+                cardDiv.appendChild(fileDiv);
+                metaDiv.appendChild(cardDiv);
+
+                filesContainer.appendChild(metaDiv);
+            });
+        }
+
+        fetchFiles(getDict(files, "resources"));
     });
-    
-    get_files("/~r09922104/shared_directory/");
-    
-    // 
+
 });
